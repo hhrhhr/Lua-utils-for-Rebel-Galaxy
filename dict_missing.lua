2ced3ee9f7b608a131101ec819ea74ab1a93e8ed
Dmitry Zaitsev <hhrhhr@users.noreply.github.com>
2015-11-01 06:39:23 +0300

    Update dict_missing.lua

--- dict_missing.lua
+++ dict_missing.lua
@@ -4,12 +4,17 @@ local miss = {
 1049256232, -- x2   <STRING>$1049256232:CENTER           \UI\
 1331837003, -- x37  <INTEGER>$1331837003:1               \MISSIONS\ \CONVERSATION\
 1371113880, -- x10  <STRING>$1371113880:{link:27725}MEDIA/MENUS/target_weaponplatform.tga
-2359553874, -- x1   <STRING>$2359553874:{link:4294967295} <-- null link
+2359553874, -- x many <STRING>$2359553874:{link:4294967295} <-- null link
 3196693291, -- x2   <STRING>$3196693291:CENTER           \UI\
 3196709755, -- x2   <STRING>$3196709755:CENTER           \UI\
 3200048657, -- x24  <STRING>$3200048657:{link:28739}SHOW \UI\
+
+-- probably devs mistake, must be a PAD_MESH_LOD (as in other files)
 3865501517, -- x1   <STRING>$3865501517:{link:27644}MEDIA/SHIPS/RETRO_TURRETS/small_2axis_1barrelpad_lod.mdl \SHIPS\WEAPON\
+
+-- probably devs mistake, must be a BROADSIDE_RANGE_BONUS (as in other files)
 3876458999, -- x1   <FLOAT>$3876458999:5.0               \SHIPS\
+
 3963315317, -- x23  <STRING>$3963315317:{link:20525}LINE \GRAPHS\
 4111501245, -- x2   <FLOAT>$4111501245:4.5               \SHIPS\
 4220792938, -- x863 <FLOAT>$4220792938:10.0              \SHIPS\, \OBJECTS\
