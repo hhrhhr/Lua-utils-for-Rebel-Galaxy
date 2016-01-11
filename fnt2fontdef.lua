cfa981c0be41c2150e7eb01e73980144f39af911
Dmitry Zaitsev <hhrhhr@gmail.com>
2016-01-11 10:04:29 +0300

    [alpha] font generator

---
+++ fnt2fontdef.lua
@@ -0,0 +1,68 @@
+assert("Lua 5.3" == _VERSION)
+
+local in_file = assert(arg[1], "no input")
+local out_file = arg[2]
+
+local r = assert(io.open(in_file))
+
+local t = {}
+
+-- first and second lines
+for i = 1, 2 do
+    local line = r:read("*l")
+    for k, v in string.gmatch(line, "(%w+)=(%w+)") do
+        t[k] = tonumber(v)
+    end
+end
+
+-- page line
+t.page = {}
+for i = 1, t.pages do
+    local tt = {}
+    local line = r:read("*l")
+    for k, v in string.gmatch(line, "(%w+)=(%w+)") do
+        tt[k] = tonumber(v)
+    end
+    table.insert(t.page, tt)
+end
+
+-- char count
+local line = r:read("*l")
+for k, v in string.gmatch(line, "(%w+)=(%w+)") do
+    t[k] = tonumber(v)
+end
+
+-- chars
+t.char = {}
+for i = 1, t.count do
+    local tt = {}
+    local line = r:read("*l")
+    for k, v in string.gmatch(line, "(%a+)=([-%d]+)") do
+        tt[k] = tonumber(v)
+    end
+    table.insert(t.char, tt)
+end
+
+r:close()
+
+io.write("Verdana\\Trivial\n")
+
+io.write([[
+{
+	type	image
+	source	media/fonts/]])
+io.write("Verdana\\Trivial\n")
+io.write("	uoffset	0\n")
+
+local w = t.scaleW
+local h = t.scaleH
+
+for k, v in ipairs(t.char) do
+    local id = v.id --+ 32
+    local x1, y1 = v.x / w, v.y / h
+    local x2, y2 = (v.x + v.width) / w, (v.y + v.height) / h
+    local fmt = "\tglyph u%d %f %f %f %f\n"
+    io.write(fmt:format(id, x1, y1, x2, y2))
+end
+
+io.write("}\n")
