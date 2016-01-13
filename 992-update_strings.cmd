d158f4a4c73625fba2c8f74f6f1d56df052a7c43
Dmitry Zaitsev <hhrhhr@gmail.com>
2016-01-13 22:20:09 +0300

    oops...

--- 992-update_strings.cmd
+++ 992-update_strings.cmd
@@ -6,6 +6,8 @@ if not exist "%WORK%" goto eof
 if not exist "%TRANSLATE%_DAT" got eof
 if not exist "%WORK%\t_list_duplicated_ids.lua" goto eof
 
+set TMPFILE=%WORK%\%RANDOM%
+
 for /r "%TRANSLATE%_DAT" %%i in (*.LUA) do (
     lua lua2dat.lua "%%i" "%TMPFILE%" "%WORK%\t_list_duplicated_ids.lua"
     lua dat2lua.lua "%TMPFILE%" "%%i"
